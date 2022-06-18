245c877ddbfe5d903dff8d92ca868065ec1edce1
Satria-WS <extencezone02@gmail.com>
2022-06-18 12:56:20 +0700

    change folder

--- FRONT-END/script.js
+++ FRONT-END/script.js
@@ -1,32 +1,32 @@
-//View
+//##View
 var map = L.map("map").setView([-6.41766, 106.804962], 11);
 
-//tileLayer
+//##tileLayer
 var tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 14,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-//marker
+//##marker
 var marker = L.marker([-6.440859, 106.772175]).addTo(map);
 
-//circle
+//##circle
 var circle = L.circle([-6.397872, 106.802044], {
   color: "red",
   fillColor: "#f03",
   fillOpacity: 0.5,
   radius: 399,
-}).addTo(map);
+}); //.addTo(map);
 
-//polygon
+//##polygon
 var polygon = L.polygon([
   [-6.405634, 106.8117],
   [-8.374562, 115.125732],
   [51.51, -0.047],
-]).addTo(map);
+]); //.addTo(map);
 
-//popup
+//##popup
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
@@ -36,9 +36,8 @@ var popup = L.popup();
 const onMapClick = (event) => {
   popup
     .setLatLng(event.latlng)
-    .setContent("You clicked the map at " + event.latlng.toString())
+    .setContent("coordinates " + event.latlng.toString())
     .openOn(map);
 };
 
 map.on("click", onMapClick);
-
